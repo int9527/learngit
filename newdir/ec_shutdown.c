7d22bcc190f645313bf164d633bac2209dd17f62
int9527 <ruyi.zhang@eaecis.com>
2024-09-08 13:30:15 +0800

    Create ec_shutdown.c

---
+++ newdir/ec_shutdown.c
@@ -0,0 +1,45 @@
+#include <stdio.h>  
+#include <stdint.h>  
+#include <fcntl.h>  
+#include <unistd.h>  
+#include <stdint.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <fcntl.h>   
+#include <sys/mman.h>  
+#include <sys/types.h>  
+#include <sys/stat.h>  
+#define EC_OBF 0x01
+#define EC_IBF 0x02
+void ShutDown( ){	
+    int fd;
+  //以可读可写，同步写入模式打开/dev/mem
+  fd = open ("/dev/mem", O_RDWR | O_SYNC);
+  if (fd == -1)
+    {
+      printf ("open /dev/mem fail!\n");
+      return (-1);
+    }
+    //使用mmap函数进行映射
+    unsigned char *map_base;
+    map_base =mmap (NULL,1, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0x20000000);
+    //对62 66IO进行映射
+    unsigned char *LPC_IO_62=(volatile unsigned char*)(map_base + 0x62);
+    unsigned char *LPC_IO_66=(volatile unsigned char*)(map_base + 0x66);
+    //printf("111\n");
+    // unsigned char value_read1=*LPC_IO_62;
+    // unsigned char value_read=*LPC_IO_66;
+    //printf("0x%02x\n",value_read);
+    //确认输入缓冲区为空，并向66端口输入0x81，说明要写EC RAM
+    while ((*LPC_IO_66 & EC_IBF));
+    *LPC_IO_66=0x81;
+
+    //printf("0x%02x\n",value_read);
+    //确认输入缓冲区为空，并向62端口输入要修改的地址0x0A
+    while ((*LPC_IO_66 & EC_IBF));
+    *LPC_IO_62=0x0A;
+    //确认输入缓冲区为空，修改0x0A地址的值为0x08（重启）
+    while ((*LPC_IO_66 & EC_IBF));
+    //usleep(10000);
+    *LPC_IO_62=0x04;
+}
